e56cdf12d590ea467547c980f82478db24da6e35
joye2003 <leoscchou@gmail.com>
2022-04-18 13:48:41 +0800

    202204182

--- src/javaOOP.java
+++ src/javaOOP.java
@@ -1,5 +1,13 @@
 public class javaOOP {
     public static void main(String[] args) {
-        System.out.printf("hello");
+        Vehicle car1 = new Vehicle();
+        Vehicle car ;
+        int[] x = new int[]{2,3,4,5,6};
+        int y = 3;
+        for (int a:x) {
+            System.out.print(a+" ");
+        }
+        System.out.println("\n"+y);
+        System.out.println("============================================");
     }
 }
