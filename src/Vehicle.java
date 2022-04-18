8cc1f2a5b38a556411073da95402398eb08d0a64
joye2003 <leoscchou@gmail.com>
2022-04-18 14:13:27 +0800

    202204183

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,3 +1,9 @@
 public class Vehicle {
-
+    int wheel;
+    public void setWheel(int n){
+        wheel = n;
+    }
+    public int getWheel(){
+        return wheel;
+    }
 }
--- src/javaOOP.java
+++ src/javaOOP.java
@@ -1,6 +1,7 @@
 public class javaOOP {
     public static void main(String[] args) {
         Vehicle car1 = new Vehicle();
+        Vehicle car2 = new Vehicle();
         Vehicle car ;
         int[] x = new int[]{2,3,4,5,6};
         int y = 3;
@@ -9,5 +10,11 @@ public class javaOOP {
         }
         System.out.println("\n"+y);
         System.out.println("============================================");
+        car1.setWheel(3);
+        car2.setWheel(4);
+        car=car1;
+        System.out.println("car1 has "+car1.getWheel()+" wheels.");
+        System.out.println("car2 has "+car2.getWheel()+" wheels.");
+        System.out.println("car has "+car.getWheel()+" wheels.");
     }
 }
